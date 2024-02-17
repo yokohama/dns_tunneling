2aa3f9c19f2ccb7ee809148d42d0f34e3549310d
yokohama <yuhei.yokohama@gmail.com>
2024-02-17 18:28:48 +0900

    init

--- bin/decode.sh
+++ bin/decode.sh
@@ -7,11 +7,13 @@ IFS='/' read -r -a array <<< "$1"
 length=${#array[@]}
 base64_full_filename=${array[length-1]}
 
+prefix=`echo $base64_full_filename | awk -F'_' '{print $1}'`
+echo $prefix
 base64_filename=`echo $base64_full_filename | awk -F'_' '{print $2}'`
 filename=`echo $base64_filename | base64 -d`
 
 sanitize_filename=`echo $filename | sed 's|/|__|g'`
 
-cat $file_path | base64 -d > outfile/$sanitize_filename
+cat $file_path | base64 -d > outfile/${prefix}-${sanitize_filename}
 
 exit
